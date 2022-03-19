8d700ff79a2457fa70dc69ea5cdc820dc0de58a2
Nithish-CC <88723055+Nithish-CC@users.noreply.github.com>
2022-03-19 16:39:25 +0530

    posts folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PostCreate from "./PostCreate";
+import PostCreate from "./posts/PostCreate";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default () =>{
